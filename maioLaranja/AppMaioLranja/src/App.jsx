44563dffdb9de269dbb06e607c3e70a12d75fe15
Pedrovicentte <pedrohenei092@gmail.com>
2023-06-13 13:42:14 -0300

    mini-curso Reactt

---
+++ maioLaranja/AppMaioLranja/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css';
+import Texto from './components/texto';
+import Imagem from './components/imagem';
+
+
+function App() {
+
+
+  return (
+    <div>
+
+      <h1 id="h1">18 de Maio </h1>
+      <Texto/>
+      <Imagem/>
+
+    </div>
+ 
+  );
+}
+
+export default App;
